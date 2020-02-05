34d681473ff1f5c2c9cee24cabd10c748cbd8c92
MontoyaR <ramontoya7474@eagle.fgcu.edu>
2020-02-05 15:14:17 -0500

    Imported SEOExample.csv and used the Scanner class to read and print the contents of the file.

--- src/main/java/io/github/MontoyaR/Main.java
+++ src/main/java/io/github/MontoyaR/Main.java
@@ -1,10 +1,36 @@
 package io.github.MontoyaR;
 import com.opencsv.CSVReader;
 
-public class Main {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+// You will definitely need AT LEAST ONE MORE IMPORT
 
+class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello.");
+        // Reads a csv file and outputs the results.
+        try {
+            Scanner scanner = new Scanner(new File("src/Data/SEOExample.csv"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (!line.equals("")){
+                    System.out.println(line);
+                }
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // The repair file.
+        Path repairsFile = Paths.get("src/Data/SEOExample.csv");
+
+        // ADD CODE HERE
+        boolean pathExists = Files.exists(repairsFile);
     }
 }
